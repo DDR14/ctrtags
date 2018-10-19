89b9eb769813f1577612785a0a2459acb751edf6
DDR14 <44149923+DDR14@users.noreply.github.com>
2018-10-19 09:23:06 +0800

    Create about-us.php

---
+++ inc/about-us.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="container">
+        <div class="col-lg-12">
+            <img class="img-responsive" src="../build/img/ctrtagflatlogo.png" width="300px" height="300px" alt="">
+            <div class="intro-text">
+                <span class="name">About CTR Tags</span>
+                <hr class="star-light">
+                <span class="skills"><h2>The mission of CTR Tags is to help motivate children to come to know that they are children of God and that he loves them.</h2></span>
+                <a href="about-us/our-family-business.php" class="btn btn-lg btn-outline" width="200px">
+                    Read More 
+                </a>
+            </div>
+        </div>
+    </div>
+</header>
+    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
